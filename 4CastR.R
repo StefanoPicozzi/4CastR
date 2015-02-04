6fb8a49f7955ba536c7c1ded7a0e82e64ba55273
Stefano Picozzi <StefanoPicozzi@gmail.com>
2015-02-04 15:06:34 +1100

    x

--- 4CastR.R
+++ 4CastR.R
@@ -17,7 +17,7 @@ library("rgl")
 library("xlsx")
 require("grid")
 
-setwd("/Users/stefanopicozzi/Google Drive/4Cast/")
+setwd("/Users/stefanopicozzi/Google Drive/4CastR/")
 
 # The top 20 ASX stocks
 zLabel <- c("AMP", "ANZ", "BHP", "BXB",   "CBA", "CSL", "IAG",	"MQG", "NAB", "ORG",	"QBE", "RIO", "SCG",	"SUN", "TLS", "WBC",	"WES", "WFD", "WOW", "WPL")	
@@ -36,8 +36,9 @@ xMin <- 0
 yMin <- 0
 xMax <- 10
 yMax <- 10
+size <- 50
 config <- data.frame(xAxisTitle, yAxisTitle, chartTitle, 
-                     xIntercept, yIntercept, xMin, yMin, xMax, yMax, stringsAsFactors=FALSE)
+                     xIntercept, yIntercept, xMin, yMin, xMax, yMax, size, stringsAsFactors=FALSE)
 
 get4Cast <- function(config, data) {
 
@@ -52,7 +53,8 @@ get4Cast <- function(config, data) {
    c10Label <- "Convert"
    c01Label <- "Counsel"
    c11Label <- "Close"
-      
+   subTotalOffset <- 0.35
+   
    data <-cbind(data, yJitter=data[,"y"]+(rnorm(N)/2), xJitter=data[,"x"]+(rnorm(N)/2))
 
    data[,"z"] <- as.numeric(data[,"z"])
@@ -82,7 +84,7 @@ get4Cast <- function(config, data) {
    
       # Plotting
       geom_point(aes(size=z), colour="blue", fill="blue", shape=21, alpha=0.5) +   
-      scale_size_area(max_size=70, guide=FALSE) +   
+      scale_size_area(max_size=config$size, guide=FALSE) +   
       geom_text(aes(y=yLabel, x=xLabel, label=zLabel), size=4, hjust=0, vjust=0, color="black", face="bold") +
       geom_text(aes(y=yJitter, x=xJitter+0.2, label=zMoney), size=3, hjust=1, vjust=1, color="white", face="bold") +
    
@@ -98,7 +100,7 @@ get4Cast <- function(config, data) {
 
       # Chart Settings
       theme(plot.background = element_rect(fill = 'grey')) +
-      ggtitle(config$chartTitle) + theme(plot.title=element_text(size=24, face="bold")) +
+      ggtitle(paste(config$chartTitle, " [ Total=", printMoney(total), " ]", sep="")) + theme(plot.title=element_text(size=20, face="bold")) +
       theme(legend.position=c(0.1,0.45)) + theme(legend.key.height=unit(1,"cm")) +
 
       # Grid Layout
@@ -114,12 +116,13 @@ get4Cast <- function(config, data) {
       annotate("text", x=config$xMin, y=config$yMin, label=c00Label, size=6, fontface="bold.italic", hjust=0) +
       annotate("text", x=config$xMin, y=config$yMax, label=c01Label, size=6, fontface="bold.italic", hjust=0) +
 
-      annotate("text", x=config$xMax, y=config$yMax-0.25, label=printMoney(c11Total), size=4, fontface="bold", hjust=1) +
-      annotate("text", x=config$xMax, y=config$yMin+0.25, label=printMoney(c10Total), size=4, fontface="bold", hjust=1) +
-      annotate("text", x=config$xMin, y=config$yMin+0.25, label=printMoney(c00Total), size=4, fontface="bold", hjust=0) +
-      annotate("text", x=config$xMin, y=config$yMax-0.25, label=printMoney(c01Total), size=4, fontface="bold", hjust=0) +
-
-      annotate("text", x=config$xMin, y=config$yMin+1.5, label=paste("Grand Total=", printMoney(total), separator=""), size=6, fontface="bold", hjust=0)
+      annotate("text", x=config$xMax, y=config$yMax-subTotalOffset, label=printMoney(c11Total), size=4, fontface="bold", hjust=1) +
+      annotate("text", x=config$xMax, y=config$yMin+subTotalOffset, label=printMoney(c10Total), size=4, fontface="bold", hjust=1) +
+      annotate("text", x=config$xMin, y=config$yMin+subTotalOffset, label=printMoney(c00Total), size=4, fontface="bold", hjust=0) +
+      annotate("text", x=config$xMin, y=config$yMax-subTotalOffset, label=printMoney(c01Total), size=4, fontface="bold", hjust=0) 
+      
+      #+
+      #annotate("text", x=config$xMin, y=config$yMin+1.5, label=paste("Grand Total=", printMoney(total), separator=""), size=6, fontface="bold", hjust=0)
    
    
 }
